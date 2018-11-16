88abbaf0c24633bbc495a2ac5396d49d50a6103d
Bruno Correia <brunocs@msn.com>
2018-11-15 23:54:20 -0200

    Add some es6 oop examples

---
+++ examples.js
@@ -0,0 +1,9 @@
+const primitive = "This is a primitive string";
+const object = new String("This is an object declared as string");
+
+// console.log(typeof primitive, primitive);
+// console.log(typeof object, object);
+
+//// Window is the absolute parent object of the browser
+// console.log(window);
+// console.log(window.navigator);
